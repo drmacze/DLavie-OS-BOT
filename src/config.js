fd6bb3b052cd1fcad51b600df7e28d1774575ac3
Damcza Dev <drma801@gmail.com>
2026-06-05 01:05:13 +0700

    Change default bot name to 'DLV BOT' (only name change)

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const config = {
   botNumber: process.env.BOT_NUMBER || '6285725483343',
   ownerNumber: process.env.OWNER_NUMBER || '62882007437216',
-  botName: process.env.BOT_NAME || 'DLAVIE BOT',
+  botName: process.env.BOT_NAME || 'DLV BOT',
 };
 
 module.exports = config;
